70bb7d6e7075ca984ddf8896c2897faa3c57f32b
demola234 <kolawoleoluwasegun567@gmail.com>
2025-03-16 22:24:10 +0100

    Update FlutterBunny formula with direct URLs and SHA256 for macOS ARM64 and Intel

--- flutter_bunny.rb
+++ flutter_bunny.rb
@@ -2,16 +2,16 @@ class FlutterBunny < Formula
     desc "Flutter Bunny: A CLI tool for Flutter development"
     homepage "https://github.com/demola234/flutter_bunny_cli"
     license "MIT"
-    version "{{VERSION}}"
+    version "1.0.6"
   
     on_macos do
       on_arm do
-        url "{{MACOS_ARM64_URL}}"
-        sha256 "{{MACOS_ARM64_SHA256}}"
+        url "https://github.com/demola234/flutter_bunny_cli/releases/download/v1.0.0/flutter_bunny-v1.0.0-macos-arm64.tar.gz"
+        sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
       end
       on_intel do
-        url "{{MACOS_X64_URL}}"
-        sha256 "{{MACOS_X64_SHA256}}"
+        url "https://github.com/demola234/flutter_bunny_cli/releases/download/v1.0.0/flutter_bunny-v1.0.0-macos-x64.tar.gz"
+        sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
       end
     end
   
