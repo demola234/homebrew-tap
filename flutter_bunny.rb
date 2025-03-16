0aafa00745aaa21e4c964032d8ccac254b7aac98
demola234 <kolawoleoluwasegun567@gmail.com>
2025-03-16 22:30:48 +0100

    Update FlutterBunny formula URLs and SHA256 for version 1.0.6; add empty Downloading and Fetching files

--- flutter_bunny.rb
+++ flutter_bunny.rb
@@ -6,11 +6,11 @@ class FlutterBunny < Formula
   
     on_macos do
       on_arm do
-        url "https://github.com/demola234/flutter_bunny_cli/releases/download/v1.0.0/flutter_bunny-v1.0.0-macos-arm64.tar.gz"
-        sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+        url "https://github.com/demola234/flutter_bunny_cli/releases/download/v1.0.6/flutter_bunny-v1.0.0-macos-arm64.tar.gz"
+        sha256 "ed3a59879720749306dec5eb1417bb2f8817a923d48d8da488426c7a791bd05c"
       end
       on_intel do
-        url "https://github.com/demola234/flutter_bunny_cli/releases/download/v1.0.0/flutter_bunny-v1.0.0-macos-x64.tar.gz"
+        url "https://github.com/demola234/flutter_bunny_cli/releases/download/v1.0.6/flutter_bunny-v1.0.0-macos-x64.tar.gz"
         sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
       end
     end
