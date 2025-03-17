4e71cd79a86885a56c9c7163f055b213beb6154f
demola234 <demola234@users.noreply.github.com>
2025-03-17 07:15:13 +0000

    Update formula to v1.0.6-beta.9

--- flutter_bunny.rb
+++ flutter_bunny.rb
@@ -2,16 +2,16 @@ class FlutterBunny < Formula
   desc "Flutter Bunny: A CLI tool for Flutter development"
   homepage "https://github.com/demola234/flutter_bunny_cli"
   license "MIT"
-  version "1.0.4"
+  version "1.0.6-beta.9"
 
   on_macos do
     on_arm do
-      url "https://github.com/demola234/flutter_bunny_cli/archive/refs/tags/v1.0.4.tar.gz"
-      sha256 "UPDATE_WITH_ACTUAL_HASH"
+      url "https://github.com/demola234/flutter_bunny_cli/archive/refs/tags/v1.0.6-beta.9.tar.gz"
+      sha256 "185e0e77e82dd1ebbb90efeac04abfd59c5ef4a771c40fb3990fc6ff74587007"
     end
     on_intel do
-      url "https://github.com/demola234/flutter_bunny_cli/archive/refs/tags/v1.0.4.tar.gz"
-      sha256 "UPDATE_WITH_ACTUAL_HASH"
+      url "https://github.com/demola234/flutter_bunny_cli/archive/refs/tags/v1.0.6-beta.9.tar.gz"
+      sha256 "185e0e77e82dd1ebbb90efeac04abfd59c5ef4a771c40fb3990fc6ff74587007"
     end
   end
 
